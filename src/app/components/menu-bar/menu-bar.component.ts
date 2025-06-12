176da9c96b40cb18a27d464a699e8e7b7077bac8
Leefrancinaldo <francinaldosb@outlook.com.br>
2025-06-12 09:59:30 -0300

    testes deply 10

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css'],
   standalone: true,
-  imports: []
+  imports: [RouterLink]
 })
 export class MenuBarComponent implements OnInit {
 
